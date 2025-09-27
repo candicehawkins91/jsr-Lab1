2c09ada1e65c068a5e40532e5d8abc6ce1fce52c
Candice Hawkins <candicehawkins91git config --global user.email candicehawkins91@gmail.com>
2025-09-27 16:26:41 -0500

    adding to html

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-function verse1
-    console.log("I'm gonna make a change")
-    console.log("For once in my life")    
-    console.log("It's gonna feel real good")
-    console.log("Gonna make a difference, gonna make it right")
-    console.log("As I turn up the collar on my favorite winter coat")
+
+function manInTheMirror(){
+
+    
+    function verse1(){
+
+        console.log("I'm gonna make a change")
+        console.log("For once in my life")    
+        console.log("It's gonna feel real good")
+        console.log("Gonna make a difference, gonna make it right")
+        console.log("As I turn up the collar on my favorite winter coat")
     console.log("This wind is blowin' my mind")
     console.log("I see the kids in the street with not enough to eat")
     console.log("Who am I to be blind, pretending not to see their needs?")
@@ -12,6 +17,7 @@ function verse1
     console.log("They follow each other on the wind, ya know")
     console.log("Cause they got nowhere to go")
     console.log("That's why I want you to know")        
+}
 
 function chorus(){
     console.log("I'm starting with the man in the mirror")
@@ -25,16 +31,18 @@ function addOn(){
 
     console.log("Na-na-na, na-na-na, na-na-na-na Uh-huh")
 }
+ function verse2(){
 
-console.log("I've been a victim of a selfish kinda love")
-console.log("It's time that I realize")
-console.log("There are some with no home, not a nickel to loan")
-console.log("Could it be really me pretending that they're not alone?")
-console.log("A willow deeply scarred, somebody's broken heart")
-console.log("And a washed-out dream (a washed-out dream)")
-console.log("They follow the pattern of the wind, ya see")
-console.log("'Cause they got no place to be")
-console.log("That's why I'm starting with me")
+     console.log("I've been a victim of a selfish kinda love")
+     console.log("It's time that I realize")
+     console.log("There are some with no home, not a nickel to loan")
+     console.log("Could it be really me pretending that they're not alone?")
+     console.log("A willow deeply scarred, somebody's broken heart")
+     console.log("And a washed-out dream (a washed-out dream)")
+     console.log("They follow the pattern of the wind, ya see")
+     console.log("'Cause they got no place to be")
+     console.log("That's why I'm starting with me")
+    }
 
 
 I'm starting with the man in the mirror (who?)
@@ -49,40 +57,44 @@ And no message could have been any clearer
 If you wanna make the world a better place
 Take a look at yourself and then make that change
 
-I'm starting with the man in the mirror (oh, yeah)
+("I'm starting with the man in the mirror (oh, yeah)
 I'm asking him to change his ways (oh, you better change)
 No message could have been any clearer
 If you wanna make the world a better place
 Take a look at yourself and then make the change
 
-You gotta get it right while you got the time
-'Cause when you close your heart (you can't)
-Then you close your (your) mind (that man, that man)
-With the man in the mirror, oh, yeah (that man, that man)
-I'm asking him to change his ways (that man, that man, that man)
-Oh, you better change (you know, that man)
-And no message could have been any clearer
-If you wanna make the world a better place
-Take a look at yourself and then make that change
 
-Na-na-na, na-na-na, na-na-na-na
-
-console.log("(Ooh) gonna feel real good, yeah (oh, yeah)")
-console.log("Yeah, yeah, yeah, yeah (na-na-na), yeah, yeah (na-na-na, na-na-na-na)")
-console.log("Oh-oh, oh, no")
-console.log("I'm gonna make a change")
-console.log("It's gonna feel real good, shamone (change)")
-console.log("Just lift yourself, you know")
-console.log("You've got to start with yourself (yeah)")
-console.log("I gotta make that change today")
-console.log("(Man in the mirror) you've got to")
-console.log("You got to start with yourself, brother")
-console.log("(Yeah) you know")
-console.log(" I've got to be that man, that man")
-console.log("(Man in the mirror) you've got to")
-console.log("You got to move, shamone, shamone")
-console.log("You got to stand up (yeah), stand up (make that change), stand up")
-console.log("Stand up and lift yourself now (man in the mirror)")
-console.log("tGo make that change, shamone (man in the mirror)")
-console.log("You know it, you know it, you know it, you know (change)")
-console.log("Make that change") 
+function ending(){
+    console.log("You gotta get it right while you got the time")
+    console.log("'Cause when you close your heart (you can't)")
+    console.log("Then you close your (your) mind (that man, that man)")
+    console.log("With the man in the mirror, oh, yeah (that man, that man)")
+    console.log("I'm asking him to change his ways (that man, that man, that man)")
+    console.log("Oh, you better change (you know, that man)")
+    console.log("And no message could have been any clearer")
+    console.log("If you wanna make the world a better place")
+    console.log("Take a look at yourself and then make that change")
+    
+    Na-na-na, na-na-na, na-na-na-na
+    
+    console.log("(Ooh) gonna feel real good, yeah (oh, yeah)")
+    console.log("Yeah, yeah, yeah, yeah (na-na-na), yeah, yeah (na-na-na, na-na-na-na)")
+    console.log("Oh-oh, oh, no")
+    console.log("I'm gonna make a change")
+    console.log("It's gonna feel real good, shamone (change)")
+    console.log("Just lift yourself, you know")
+    console.log("You've got to start with yourself (yeah)")
+    console.log("I gotta make that change today")
+    console.log("(Man in the mirror) you've got to")
+    console.log("You got to start with yourself, brother")
+    console.log("(Yeah) you know")
+    console.log(" I've got to be that man, that man")
+    console.log("(Man in the mirror) you've got to")
+    console.log("You got to move, shamone, shamone")
+    console.log("You got to stand up (yeah), stand up (make that change), stand up")
+    console.log("Stand up and lift yourself now (man in the mirror)")
+    console.log("tGo make that change, shamone (man in the mirror)")
+    console.log("You know it, you know it, you know it, you know (change)")
+    console.log("Make that change") 
+}
+}
