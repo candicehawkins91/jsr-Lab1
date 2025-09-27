1444d86131f493e773cdd9c08185a629095eaa2a
Candice Hawkins <candicehawkins91git config --global user.email candicehawkins91@gmail.com>
2025-09-27 15:24:03 -0500

    working  on the song

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # jsr-Lab1
 Man in the mirror by Micheal Jackson is the song I choose. It's long but I love it so please work with me. Plus I need the practice. Also I am using my read me to break it down how you told me. I will you comments for lyrical purpose. Anything that is not in a comment I mean for you to read.
+
+
+
+<!--  
+(I'm gonna make a change
+For once in my life
+It's gonna feel real good
+Gonna make a difference, gonna make it right
+As I turn up the collar on my favorite winter coat
+This wind is blowin' my mind
+I see the kids in the street with not enough to eat
+Who am I to be blind, pretending not to see their needs?
+A summer's disregard, a broken bottle top
+And a one man's soul
+They follow each other on the wind, ya know
+'Cause they got nowhere to go
+That's why I want you to know
+I'm starting with the man in the mirror
+I'm asking him to change his ways
+And no message could have been any clearer
+If you wanna make the world a better place
+Take a look at yourself and then make a change
+Na-na-na, na-na-na, na-na-na-na
+Uh-huh
+I've been a victim of a selfish kinda love
+It's time that I realize
+There are some with no home, not a nickel to loan
+Could it be really me pretending that they're not alone?
+A willow deeply scarred, somebody's broken heart
+And a washed-out dream (a washed-out dream)
+They follow the pattern of the wind, ya see
+'Cause they got no place to be
+That's why I'm starting with me
+I'm starting with the man in the mirror (who?)
+I'm asking him to change his ways (who?)
+And no message could have been any clearer
+If you wanna make the world a better place
+Take a look at yourself and then make a change
+I'm starting with the man in the mirror (who?)
+I'm asking him to change his ways (who?)
+And no message could have been any clearer
+If you wanna make the world a better place
+Take a look at yourself and then make that change
+I'm starting with the man in the mirror (oh, yeah)
+I'm asking him to change his ways (oh, you better change)
+No message could have been any clearer
+If you wanna make the world a better place
+Take a look at yourself and then make the change
+You gotta get it right while you got the time
+'Cause when you close your heart (you can't)
+Then you close your (your) mind (that man, that man)
+With the man in the mirror, oh, yeah (that man, that man)
+I'm asking him to change his ways (that man, that man, that man)
+Oh, you better change (you know, that man)
+And no message could have been any clearer
+If you wanna make the world a better place
+Take a look at yourself and then make that change
+Na-na-na, na-na-na, na-na-na-na
+(Ooh) gonna feel real good, yeah (oh, yeah)
+Yeah, yeah, yeah, yeah (na-na-na), yeah, yeah (na-na-na, na-na-na-na)
+Oh-oh, oh, no
+I'm gonna make a change
+It's gonna feel real good, shamone (change)
+Just lift yourself, you know
+You've got to start with yourself (yeah)
+I gotta make that change today
+(Man in the mirror) you've got to
+You got to start with yourself, brother
+(Yeah) you know
+I've got to be that man, that man
+(Man in the mirror) you've got to
+You got to move, shamone, shamone
+You got to stand up (yeah), stand up (make that change), stand up
+Stand up and lift yourself now (man in the mirror)
+Go make that change, shamone (man in the mirror)
+You know it, you know it, you know it, you know (change)
+Make that change )
+--> 
---
+++ index.js
@@ -0,0 +1,81 @@
+
+    console.log("I'm gonna make a change")
+    console.log("For once in my life")    
+    console.log("It's gonna feel real good")
+    console.log("Gonna make a difference, gonna make it right")
+    console.log("As I turn up the collar on my favorite winter coat")
+    console.log("This wind is blowin' my mind")
+    console.log("I see the kids in the street with not enough to eat")
+    console.log("Who am I to be blind, pretending not to see their needs?")
+    console.log("A summer's disregard, a broken bottle top")
+    console.log("And a one man's soul")
+    console.log("They follow each other on the wind, ya know")
+    console.log("Cause they got nowhere to go")
+    console.log("That's why I want you to know")        
+
+function chorus(){
+    console.log("I'm starting with the man in the mirror")
+    console.log("I'm asking him to change his ways")
+    And no message could have been any clearer
+    If you wanna make the world a better place
+    Take a look at yourself and then make a change
+}
+
+function addOn(){
+    Na-na-na, na-na-na, na-na-na-na
+    Uh-huh
+}
+
+I've been a victim of a selfish kinda love
+It's time that I realize
+There are some with no home, not a nickel to loan
+Could it be really me pretending that they're not alone?
+A willow deeply scarred, somebody's broken heart
+And a washed-out dream (a washed-out dream)
+They follow the pattern of the wind, ya see
+'Cause they got no place to be
+That's why I'm starting with me
+I'm starting with the man in the mirror (who?)
+I'm asking him to change his ways (who?)
+And no message could have been any clearer
+If you wanna make the world a better place
+Take a look at yourself and then make a change
+I'm starting with the man in the mirror (who?)
+I'm asking him to change his ways (who?)
+And no message could have been any clearer
+If you wanna make the world a better place
+Take a look at yourself and then make that change
+I'm starting with the man in the mirror (oh, yeah)
+I'm asking him to change his ways (oh, you better change)
+No message could have been any clearer
+If you wanna make the world a better place
+Take a look at yourself and then make the change
+You gotta get it right while you got the time
+'Cause when you close your heart (you can't)
+Then you close your (your) mind (that man, that man)
+With the man in the mirror, oh, yeah (that man, that man)
+I'm asking him to change his ways (that man, that man, that man)
+Oh, you better change (you know, that man)
+And no message could have been any clearer
+If you wanna make the world a better place
+Take a look at yourself and then make that change
+Na-na-na, na-na-na, na-na-na-na
+(Ooh) gonna feel real good, yeah (oh, yeah)
+Yeah, yeah, yeah, yeah (na-na-na), yeah, yeah (na-na-na, na-na-na-na)
+Oh-oh, oh, no
+I'm gonna make a change
+It's gonna feel real good, shamone (change)
+Just lift yourself, you know
+You've got to start with yourself (yeah)
+I gotta make that change today
+(Man in the mirror) you've got to
+You got to start with yourself, brother
+console.log("(Yeah) you know")
+console.log(" I've got to be that man, that man")
+console.log("(Man in the mirror) you've got to")
+console.log("You got to move, shamone, shamone")
+console.log("You got to stand up (yeah), stand up (make that change), stand up")
+console.log("Stand up and lift yourself now (man in the mirror)")
+console.log("tGo make that change, shamone (man in the mirror)")
+console.log("You know it, you know it, you know it, you know (change)")
+console.log("Make that change") 
